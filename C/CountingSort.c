68d5010231a6cd89027b23722b38ce6f8d95120e
Aldo Gutierrez <aldodario1@hotmail.es>
2020-03-31 18:21:00 -0700

    Fixed old error, fully working now

--- C/CountingSort.c
+++ C/CountingSort.c
@@ -1,91 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int findMax(int *array, int size);
-int findMin(int *array, int size);
-int * countingSort(int *input, int *output, int k, int size);
+void countingSort(int *input, int *output, int k, int size);
 void printArray(int *array, int size);
 
 /**
- * 
+ * Linear sorting for integer values
 */
-int * countingSort(int *input, int *output, int k, int size)
+void countingSort(int *input, int *output, int k, int size)
 {
-    int counter[k + 1];
+    int *counter = (int *) malloc(sizeof(int) * (k));
+    int i;
 
-    for (int i = 0; i <= k; i++)
+    for (i = 0; i < k; i++) { counter[i] = 0; }
+    
+    for (i = 0; i < size; i++) { counter[input[i]]++; }
+
+    for (i = 1; i < k; i++) { counter[i] += counter[i - 1]; }
+    
+    for (i = (size - 1); i >= 0; --i)
     {
-        counter[i] = 0;
+        output[counter[input[i]] - 1] = input[i];
+        counter[input[i]]--;
     }
-
-    for (int j = 0; j < size; j++)
-    {
-        counter[input[j]] = counter[input[j]] + 1;
-        output[j] = 0;
-    }
-
-    for (int i = 1; i < k; i++)
-    {
-        counter[i] += counter[i - 1];
-    }
-
-    for (int j = size - 1; j >= 0; j--)
-    {
-        output[counter[input[j]] - 1] = input[j];
-        counter[input[j]] -=  1;
-    }
-
-    return output;
+    return;
 }
 
 /**
- * 
+ * Returns the maximum entry in the array
+ * The reason why we return 1 more than the max is to
+ * satisfy the "arrays start at zero" rule
 */
 int findMax(int *array, int size)
 {
     int max = 0;
     for (int i = 0; i < size; i++)
     {
-        if (array[i] > max)
-        {
-            max = array[i];
-        }
+        if (array[i] > max) { max = array[i]; }
     }
-    return max;
+    return ++max;
 }
 
 /**
- * 
+ * Prints the given array
 */
 void printArray(int *array, int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; ++i)
     {
-        printf("[%d], ", array[i]);
+        printf("%d, ", array[i]);
     }
 }
 
 int main()
 {
-    printf("***START***\n");
-    int toBeSorted[] = {7, 0, 2, 0, 1, 3, 4, 6, 1, 3, 2}; //{15, 4, 8, 26, 32, 27, 51, 72, 78, 21, 2, 8, 23};
-    int size = (int) (sizeof(toBeSorted) / sizeof(int));
-    int output[size];
+    printf("***START***\n\n");
     
+    // int toBeSorted[] = {4, 2, 0, 1, 3, 7, 6, 1, 3, 2};
+    int toBeSorted[] = {15, 4, 8, 26, 32, 27, 51, 72, 78, 21, 2, 8, 23};
+    
+    int size = (sizeof(toBeSorted) / sizeof(int));
+    
+    // Preallocate the output array
+    int *output = (int *)malloc(sizeof(int) * (size));
+
     printf("Given array: ");
     printArray(toBeSorted, size);
-    
+
     printf("\n");
-    
+
     int maxValue = findMax(toBeSorted, size);
-    int * result;
-    result = countingSort(toBeSorted, output, maxValue, size);
-    
+    countingSort(toBeSorted, output, maxValue, size);
+
     printf("Sorted array: ");
 
-    printArray(result, size);
-    
-    printf("\n");
-    printf("***END***");
-    printf("\n");
+    printArray(output, size);
+
+    printf("\n\n***END***");
     return 0;
 }
